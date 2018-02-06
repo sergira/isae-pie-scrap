f75cfce9e5f14b5622d909641c73a03b6f30c173
EvgeniiMunin <munin.evgenii@gmail.com>
2018-02-06 14:48:18 +0100

    Wired Scrapper complete (with body). Evgenii

--- ScrapyProject/spiders/wired_deep2_2Meta.py
+++ ScrapyProject/spiders/wired_deep2_2Meta.py
@@ -3,10 +3,12 @@
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
 
+import timestring
 import datetime
 import scrapy
 from ScrapyProject.items import ScrapyItem
-import numpy as np
+import pytz
+import dateutil.parser
 
 class NewsSpider(scrapy.Spider):
 
@@ -14,12 +16,7 @@ class NewsSpider(scrapy.Spider):
 	allowed_domains = ['wired.com']
 
 	start_urls = ['https://www.wired.com/search/?page=1&q=rocket&size=10&sort=publishDate_tdt%20desc&types%5B0%5D=article']
-	url_list = np.empty((1,100), dtype=object)
-	temp = False
-	j = 0
 	
-	
-		
 
 	def parse(self, response):
 		
@@ -31,18 +28,28 @@ class NewsSpider(scrapy.Spider):
 			url_temp = entry.css('a::attr(href)').extract_first()	
 			next_page = response.urljoin(url_temp)		
 
-			item['url'] = url_temp	
-			
+			item['source'] = 'wired'
+			temp_string = entry.css('time::text').extract_first()
+			item['brief'] = entry.css('a').css('p.archive-item-component__desc::text').extract_first()
+			item['url'] = entry.css('a::attr(href)').extract_first()
+			item['title'] = entry.css('a').css('h2::text').extract_first()
+
+			# check time
+			now = datetime.datetime.now()
+			now  = now.strftime("%Y-%m-%dT%H:%M:%S.%f%z")
+			item['tstamp'] = now
+
+			# transfer time into ISO 8601
+			temp = timestring.Date(temp_string).date
+			item['date']  = temp.strftime("%Y-%m-%dT%H:%M:%S.%f%z")
+		
+			# request to article page
 			request = scrapy.Request(next_page, callback=self.parse_article)
 			request.meta['item'] = item
 
 			yield request
 			
 			
-			
-
-		
-
 
 	def parse_article(self, response):
 			
