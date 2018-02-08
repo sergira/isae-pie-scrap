bf7fad580aac7809ab9704cb084b456c0160ad39
sergi.rocamora.ardevol@gmail.com <sergi.rocamora.ardevol@gmail.com>
2018-02-08 11:14:40 +0100

    SpaceNews spider OK

--- ScrapyProject/spiders/spacenews.py
+++ ScrapyProject/spiders/spacenews.py
@@ -1,8 +1,3 @@
-# This package will contain the spiders of your Scrapy project
-#
-# Please refer to the documentation for information on how to create and manage
-# your spiders.
-
 import timestring
 import datetime
 import scrapy
@@ -12,25 +7,38 @@ import dateutil.parser
 
 
 class NewsSpider(scrapy.Spider):
-	#item_id = ScrapyItem()
-	name = 'spacenews'
+
+	name = 'spacenews2'
 
 
-	allowed_domains = ['http://spacenews.com']
+	allowed_domains = ['spacenews.com']
 
-	start_urls = [('http://spacenews.com/?s=propulsion&orderby=date-desc&paged=%d' % i) for i in range(1,50)]
+	start_urls = ['http://spacenews.com/?s=propulsion&orderby=date-desc&paged=1']
 
 	def parse(self, response):
-        # iterate entries
+        # iterate entries		
+
 		for entry in response.css('div.launch-article'):
             
-            #retrieve info for our current post
+            # create information container
 			item = ScrapyItem()
 
+
 			item['source'] = 'spacenews'
+
+			# retrieve publication date
 			temp_string  = entry.css('time::text').extract_first()
+			# transfer time into ISO 8601
+			temp = timestring.Date(temp_string).date
+			item['date']  = temp.strftime("%Y-%m-%dT%H:%M:%S.%f%z")
+
+			# get abstract
 			item['brief'] = entry.css('p.post-excerpt::text').extract_first()
+
+			# get article url
 			item['url'] = entry.css('h2').css('a::attr(href)').extract_first()
+
+			# get article title
 			item['title'] = entry.css('h2').css('a::text').extract_first()
 
 			# check time
@@ -38,10 +46,25 @@ class NewsSpider(scrapy.Spider):
 			now  = now.strftime("%Y-%m-%dT%H:%M:%S.%f%z")
 			item['tstamp'] = now
 
-			# transfer time into ISO 8601
-			temp = timestring.Date(temp_string).date
-			item['date']  = temp.strftime("%Y-%m-%dT%H:%M:%S.%f%z")
+			# request to article page
+			request = scrapy.Request(item['url'], callback=self.parse_article)
+			request.meta['item'] = item
 
-			yield item
+			yield request
+		
+		
+		next_url = response.css('p.infinitescroll').css('a::attr(href)').extract_first()
+		if next_url:
+			yield scrapy.Request(next_url)
 
+	def parse_article(self, response):
+		item = response.meta['item']	
 
+		text = response.css('div').css('p::text').extract()
+		# if no body ignore entry
+		if not text:
+			return
+		# clean and join text
+		item['body'] = ' '.join('\n'.join(text).split())
+
+		return item
