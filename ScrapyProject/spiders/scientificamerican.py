a1e90330e3b14e46b79366cdff77dc85b6760857
sergi.rocamora.ardevol@gmail.com <sergi.rocamora.ardevol@gmail.com>
2018-02-27 13:13:53 +0100

    Scientific american done

--- ScrapyProject/spiders/scientificamerican.py
+++ ScrapyProject/spiders/scientificamerican.py
@@ -9,21 +9,19 @@ import scrapy
 from ScrapyProject.items import ScrapyItem
 import pytz
 import dateutil.parser
+import re
 
 class NewsSpider(scrapy.Spider):
-    name = 'scientificus_Meta'
+    name = 'scientificamerican'
     allowed_domains = ['scientificamerican.com']
     start_urls = ['https://www.scientificamerican.com/search/?q=propulsion&sortby=releasedate&source=&days=']
     def parse(self, response):
         # iterate entries
-        item = ScrapyItem()
         for entry in response.css('div.listing-wide__inner'):
-            url_temp = entry.css('a::attr(href)').extract_first()
-            next_page = response.urljoin(url_temp)
+            item = ScrapyItem()
             #retrieve info for our current post
             item['source'] = 'scientificamerican'
-            temp_string = entry.css('time::text').extract_first()
-            item['date'] = entry.css('div.t_meta::text').extract_first().split(' — ')[0]
+            temp_string = entry.css('div.t_meta::text').extract_first().split(' — ')[0]
             item['brief'] = entry.css('p::text').extract_first()
             item['url'] = entry.css('h2').css('a::attr(href)').extract_first()
             item['title'] = entry.css('h2').css('a::text').extract_first()
@@ -38,7 +36,7 @@ class NewsSpider(scrapy.Spider):
             item['date']  = temp.strftime("%Y-%m-%dT%H:%M:%S.%f%z")
 
             # request to article page
-            request = scrapy.Request(next_page, callback=self.parse_article)
+            request = scrapy.Request(item['url'], callback=self.parse_article)
             request.meta['item'] = item
 
             yield request
@@ -46,12 +44,10 @@ class NewsSpider(scrapy.Spider):
 
 
     def parse_article(self, response):
-
-
-            string = response.css('article').css('div').css('p::text').extract()
-
             item = response.meta['item']
-            item['body'] = string
+
+            text = response.css('article').css('div').css('p::text').extract()
+            item['body'] = ' '.join(re.sub('<[^>]*>', ' ', '\n'.join(text)).split())
             return item
 
 
